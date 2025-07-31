fbfce19421becd0b0406643acb344f40eade8928
PraveenD333 <praveendhane97@gmail.com>
2025-07-31 14:38:11 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://praveen-chatapp.vercel.app/',
+  origin: 'https://praveen-chatapp.vercel.app',
   methods:['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders:['Content-Type','Authorization'],
   credentials:true
