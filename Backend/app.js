ad5cb337f5125971b66fff027ca587ca2cc7440c
PraveenD333 <praveendhane97@gmail.com>
2025-07-31 15:16:48 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ import messagerouter from './Routes/message.routes.js';
 const app = express();
 
 
-app.use(express.json());
+app.use(express.json({limit:"4mb"}));
 
 app.use(cors({
   origin: 'https://praveen-chatapp.vercel.app',
